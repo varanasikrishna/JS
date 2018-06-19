c2fad1da1885e5e340f48c453beeb1e2226b7887
Krishna V D S <krishnavds@virtusa.com>
2018-06-19 12:58:27 +0530

    moved

--- week4/Day4/skill-page-in-react/src/App.js
+++ week4/Day4/skill-page-in-react/src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-import landingPage from '../page/landing-page';
+import LandingPage from './page/landing-page';
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 class App extends Component {
   render() {
     return (
-      <landingPage/>
+      <LandingPage />
     );
   }
 }
